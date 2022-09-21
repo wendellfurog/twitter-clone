f57a64f20a9ce413c2516983847f3698036c8c49
wtfurog_ <84697027+wendellfurog@users.noreply.github.com>
2022-09-21 13:23:41 +0800

    initial commit

---
+++ src/TweetBox.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './TweetBox.css';
+import { Avatar, Button } from '@material-ui/core';
+import avatar from './avatar.jpg';
+
+function TweetBox() {
+  return (
+    <div className='tweetBox'>  
+      <form>
+        <div className='tweetBox_input'>
+          <Avatar src={avatar} alt="my pic"/>
+          <input placeholder="What's happening?" type='text' />
+        </div>
+        <input 
+          className='tweetBox_imageInput'
+          placeholder="Optional: Enter image URL" 
+          type='text'
+        />
+        <Button className='tweetBox_tweetButton'>Tweet</Button>
+      </form>
+    </div>
+  )
+}
+
+export default TweetBox
