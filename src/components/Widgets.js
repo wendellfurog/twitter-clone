e11533d55dc91c2fcc2d4dfcaa9ec38fb6e01000
wtfurog_ <84697027+wendellfurog@users.noreply.github.com>
2022-09-22 16:13:31 +0800

    commit

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
+import "../styles/Widgets.css";
+import {
+  TwitterTimeLineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed
+} from 'react-twitter-embed'
 
 function Widgets() {
   return (
     <div className='widgets'>
       <div className='widgets__input'>
-      <SearchIcon className='widgets__searchIcon' />
-      <input placeholder='Search Twitter' type='text' />
+        <SearchIcon className='widgets__searchIcon' />
+        <input placeholder='Search Twitter' type='text' />
+      </div>
+      <div className='widgets__widgetContainer'>
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={'1572840350116646912'} />
       </div>
     </div>
   )
